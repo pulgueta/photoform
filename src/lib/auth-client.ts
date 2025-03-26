88e12f4cc2206daeefead156b86fc006378dcd98
pulgueta <retardix456@gmail.com>
2025-03-26 01:59:05 -0500

    feat: update auth client to use local URL in development environment

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,8 +3,13 @@ import { passkeyClient } from "better-auth/client/plugins";
 import { oneTapClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
+const localUrl = "http://localhost:3000";
+
 const authClient = createAuthClient({
-  baseURL: env.URL,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? localUrl
+      : `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`,
   plugins: [
     passkeyClient(),
     oneTapClient({
