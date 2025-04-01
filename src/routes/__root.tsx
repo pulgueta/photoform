f539b0935f5d8c057c3713ea415d1c4fb0907114
pulgueta <retardix456@gmail.com>
2025-04-01 02:56:42 -0500

    feat: implement dynamic Content-Security-Policy in head function and update favicon links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -40,45 +40,50 @@ export const Route = createRootRouteWithContext<RouteContext>()({
 
     return { user };
   },
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      ...seo({
-        title: "Phormat - Elevate your photoshoots with AI recommendations",
-        description:
-          "Create AI portraits and get tips and recommendations for your next photoshoots with your clients.",
-        keywords: "AI, photoshoots, recommendations, portraits",
-      }),
-    ],
-    links: [
-      { rel: "stylesheet", href: appCss },
-      {
-        rel: "apple-touch-icon",
-        sizes: "180x180",
-        href: "/apple-touch-icon.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        href: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        href: "/favicon-16x16.png",
-      },
-      { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
-      { rel: "icon", href: "/favicon.ico" },
-    ],
-  }),
+  head: () => {
+    const url =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : env.URL;
+
+    return {
+      meta: [
+        {
+          charSet: "utf-8",
+        },
+        {
+          httpEquiv: "Content-Security-Policy",
+          content: `script-src 'self' 'unsafe-inline' ${url} https://accounts.google.com; script-src-elem 'self' 'unsafe-inline' ${url} https://accounts.google.com;`,
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+        ...seo({
+          title: "Phormat - Elevate your photoshoots with AI recommendations",
+          description:
+            "Create AI portraits and get tips and recommendations for your next photoshoots with your clients.",
+          keywords: "AI, photoshoots, recommendations, portraits",
+        }),
+      ],
+      links: [
+        { rel: "stylesheet", href: appCss },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png",
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "196x196",
+          href: "/favicon-196.png",
+        },
+        { rel: "manifest", href: "/site.webmanifest", color: "#fffff" },
+        { rel: "icon", href: "/favicon-196.png", type: "image/png" },
+      ],
+    };
+  },
   errorComponent: (props) => {
     return (
       <RootDocument>
@@ -99,17 +104,10 @@ function RootComponent() {
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
-  const url =
-    process.env.NODE_ENV === "development" ? "http://localhost:3000" : env.URL;
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
         <HeadContent />
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content={`script-src 'self' 'unsafe-inline' ${url} https://accounts.google.com; script-src-elem 'self' 'unsafe-inline' ${url} https://accounts.google.com;`}
-        />
       </head>
       <body className="font-manrope">
         {children}
